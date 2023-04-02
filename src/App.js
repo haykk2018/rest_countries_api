ac24fc1c9f7319fd37ab49ac25108f16bad65769
hayk <haykk2018@gmail.com>
2023-04-02 12:24:27 +0400

    project "start" state - without any functionality  I used https://styled-components.com/ libs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import {Header} from "./components/Header";
+import {Main} from "./components/Main";
+import {HomePage} from "./pages/HomePage";
+import {Route, Routes} from 'react-router-dom';
+import {NotFound} from "./pages/NotFound";
+import {Details} from "./pages/Details";
+
 function App() {
     return <>
-        HELLO
+        <Header/>
+        <Main>
+            <Routes>
+                <Route exact path="/" element={<HomePage/>}/>
+                <Route path="/country/:name" element={<Details/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </Main>
     </>
 }
 
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import {applyMiddleware, compose, createStore} from '@reduxjs/toolkit'
+import thunk from 'redux-thunk'
+import axios from 'axios'
+import {rootReducer} from "./root-reducer";
+import * as api from './config';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(
+        thunk.withExtraArgument({
+            client: axios,
+            api,
+        })
+    )
+))
+
+export {store};
