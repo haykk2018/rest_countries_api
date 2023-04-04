116f8a7b175f7f5ee1618d0ae19a6ad973f3acef
hayk <haykk2018@gmail.com>
2023-04-04 11:11:50 +0400

    uncompleted project, I stopped developing because of redux dev tool worked not correct in my browsers(chrome, firefox)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,17 +3,27 @@ import { useNavigate } from 'react-router-dom';
 import { List } from '../components/List';
 import { Card } from '../components/Card';
 import { Controls } from '../components/Controls';
+import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {loadCountries} from "../store/countries/countries-actions";
+import {selectAllCountries} from "../store/countries/countries-selectors";
 
 export const HomePage = () => {
   const navigate = useNavigate();
 
-  const countries = [];
+  const countries = useSelector(selectAllCountries);
+
+  const dispatch = useDispatch();
+
+  useState(
+      dispatch(loadCountries())
+  )
 
   return (
     <>
       <Controls />
 
-      <List>
+      <List on>
             {countries.map((c) => {
               const countryInfo = {
                 img: c.flags.png,
